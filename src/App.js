a1f020146cd416354d257dc4588c350ad42106af
Deven Umrania <umraniadeven10@gmail.com>
2023-06-19 01:29:32 +0530

    update: added note

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route, NavLink } from "react-router-dom";
+import { Routes, Route, NavLink, Link } from "react-router-dom";
 
 import { Home } from "./Pages/Home/Home";
 import { Archives } from "./Pages/Archives/Archives";
@@ -15,6 +15,24 @@ function App() {
 
   return (
     <div className="App">
+      <p className="note">
+        Hi Team, I was not able to complete the task within allocated time 😥, I
+        tried it again and here's the completed task live url:{" "}
+        <Link target="_blank" to="https://mcr-two-second-try.vercel.app/">
+          Live Link
+        </Link>{" "}
+        and Github repo:{" "}
+        <Link
+          to="https://github.com/deven10/mcr-two-second-try"
+          target="_blank"
+        >
+          Code Link
+        </Link>
+      </p>
+      <p className="note">
+        What I could make during the time frame is this much only 😔
+      </p>
+      <hr />
       <nav className="navlinks">
         <NavLink style={styles} className="nav-item" to="/">
           Home
--- src/Pages/Archives/Archives.js
+++ src/Pages/Archives/Archives.js
@@ -9,9 +9,6 @@ export const Archives = () => {
       <h3>Archived Habits</h3>
       <div className="habits-wrapper">
         {archiveHabits?.map((habit) => {
-          {
-            /* console.log(habit);s */
-          }
           return (
             <div key={habit.id} className="habit">
               <p className="habit-name">Habit Name: {habit.Habit_Name}</p>
@@ -19,12 +16,6 @@ export const Archives = () => {
               <p>Goal: {habit.Goal}</p>
               <p>Time of Day: {habit.Time_of_Day}</p>
               <p>Start Date: {habit.Start_Date}</p>
-              {/* <div className="call-to-action-button">
-                <button>View Details</button>
-                <button>Edit Habit</button>
-                <button>Archive</button>
-                <button onClick={() => handleDelete(habit.id)}>Delete</button>
-              </div> */}
             </div>
           );
         })}
